950215aa88181dcf27c8e2ba28fb951f23dd7e7e
Jeremy.Bois <jeremy.bois@studio-nyx.com>
2026-02-11 13:05:43 +0100

    👽️ [Cable | MassBalance] - ST-3 - Add `anchor` mode to the list of available modes to detect stretching.

--- BulletSharp/Cable/Cable.cs
+++ BulletSharp/Cable/Cable.cs
@@ -381,8 +381,9 @@ namespace BulletSharp.SoftBody
 
 		public enum StretchRatioMode
 		{
-			Cable = 0,  // Tension ratio is computed based on the whole cable length
-			Link,       // Tension ration is computed based on the more stretched link
+			Cable = 0,  // Tension ratio is computed based on the whole cable length (same across anchors)
+			Link,       // Tension ration is computed based on the more stretched link (same across anchors)
+			Anchor,     // Tension ration is computed only at the anchor level (unique to each anchor)
 			None        // Assume mass ratio is always needed at max
 		};
 
